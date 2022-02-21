04e8c23aa10d73af28e6ffd04743d8b74d47657e
Moreno27 <alejanmm2000@gmail.com>
2022-02-21 20:26:25 +0100

    mejora codigo. implementa para intervalo con hover implementa botones para el movimiento

--- jQuery/sliderjQuery/mislider.js
+++ jQuery/sliderjQuery/mislider.js
@@ -2,42 +2,93 @@ $(document).ready(function () {
 
     let SliderModule = (function (params) {
 
-        //creamos el objeto que sera el slider
-        let slider = {}
+        //creamos el objeto 'slider'
+        let slider = {
 
-        //almacenamos en una prop el selector de jq del slider
-        slider.selectorJQ = $("#slider")
+            //almacenamos en una prop el selector de jq del slider
+            selectorJQ: $("#slider"),
 
-        //captamos los items del mismo slider
-        slider.items = {
+            items: {
 
-            panels: slider.selectorJQ.find(".slider-wrapper>li")
+                //obtenemos el array para coger los paneles
+                panels: $("#slider").find(".slider-wrapper>li")
+            },
+
+            settings: {
+                duration: null
+            },
+
+            /* CONSTRUCTOR */
+            init: function (settings) {
+
+                //creamos los controles de forma dinamica
+                creacionControles();
+
+                //adjudicamos la propiedad settings y le ponemos, o valor por defecto(8000) o valor parametrizado(settings)
+                this.settings = settings || { duration: 8000 }
+
+                SliderInit();
+
+                $("#control-buttons li").click(function () {
+
+                    //control para que no se mueva a la foto actual
+                    if (currentSlider !== $(this).index()) {
+                        cambiarPanel($(this).index())
+                    }
+                })
+            },
+
+            startSlider: function () {
+
+                /*VARIABLES NECESARIAS*/
+
+                //obtenemos los paneles del slider
+                var paneles = slider.items.panels
+
+                //boton de controles para el slider
+                let controles = $("#control-buttons li")
+
+                //si llega al final del slider(o se pasa)
+                if (nextSlider >= lengthSlider) {
+
+                    //ponemos que el siguiente slider sera el del inicio y que estamos en el ultimo slider
+                    nextSlider = 0
+                    currentSlider = lengthSlider - 1
+                }
+
+                /*EFECTOS DE MOVIMIENTO*/
+
+                //PARA LOS CONTROLES
+
+                //quitamos la clase a todos los controles
+                controles.removeClass("active")
+                //al que veremos a continuacion, le ponemos la clase para que se coloree
+                controles.eq(nextSlider).addClass("active")
+
+                //PARA LOS PANELES
+
+                //quitamos la que tenemos visible
+                paneles.eq(currentSlider).fadeOut("slow")
+                //mostramos la que se supone que hemos pinchado
+                paneles.eq(nextSlider).fadeIn("slow")
+
+                /*ACTUALIZACION DE VARIABLES*/
+
+                //actualizamos el slider actual(movimiento hacia siguiente slider)
+                currentSlider = nextSlider
+
+                //actualizamos el valor del proximo slider
+                nextSlider++
+            }
         }
 
         //DECLARAMOS VARIABLES GLOBALES
         var SliderIntervar, currentSlider = 0, nextSlider = 1, lengthSlider = slider.items.panels.length
 
-        /* CONSTRUCTOR */
-        slider.init = function (settings) {
-
-            //creamos los controles
-            creacionControles();
-
-            //adjudicamos la propiedad settings y le ponemos, o valor por defecto(8000) o valor parametrizado(settings)
-            this.settings = settings || { duration: 8000 }
-
-            //console.log("inicializado");
-            SliderInit();
-
-            $("#control-buttons li").click(function () {
-
-                //control para que no se mueva a la foto actual
-                if (currentSlider !== $(this).index()) {
-                    cambiarPanel($(this).index())
-                }
-            })
-        }
-
+        /**
+         * Funcion para la creacion de los controles para la posicion del slider
+         * de manera dinamica
+         */
         const creacionControles = () => {
 
             //creamos el string que seran los li controles
@@ -48,9 +99,9 @@ $(document).ready(function () {
 
                 //el primero siempre activo
                 if (i == 0) {
-                    controles += "<li class='active'></li>"
+                    controles += `<li class='active'></li>`
                 } else {
-                    controles += "<li></li>"
+                    controles += `<li>></li>`
                 }
             }
 
@@ -58,6 +109,11 @@ $(document).ready(function () {
             $("#control-buttons").html(controles)
         }
 
+        /**
+         * Metodo que actualiza la posicion del slider, cambiando el valor de las variables globales 
+         * en base a un indice
+         * @param {Number} indice Posicion a la que se va a mover el slider
+         */
         const cambiarPanel = (indice) => {
 
             //limpiamos el intervalo del slider
@@ -67,6 +123,9 @@ $(document).ready(function () {
 
             //paneles del slider
             let paneles = slider.items.panels
+
+            //antes de nada quitamos la animacion anterior
+            paneles.stop(true, true)
 
             //boton de controles para el slider
             let controles = $("#control-buttons li")
@@ -79,22 +138,19 @@ $(document).ready(function () {
 
             /*EFECTOS DE MOVIMIENTO*/
 
-            //PARA LOS CONTROLES
-
+            /*controles*/
             //quitamos la clase a todos los controles
             controles.removeClass("active")
             //al que hemos pulsado, le ponemos la clase para que se coloree
             controles.eq(indice).addClass("active")
 
-            //PARA LOS PANELES
-
+            /*paneles*/
             //quitamos la que tenemos visible
             paneles.eq(currentSlider).fadeOut("slow")
             //mostramos la que se supone que hemos pinchado
             paneles.eq(indice).fadeIn("slow")
 
-            //ACTUALIZACION DE VARIABLES
-
+            /*variables*/
             //actualizamos el slider actual(movimiento hacia ese slider)
             currentSlider = indice
             //actualizamos el valor del proximo slider
@@ -102,71 +158,37 @@ $(document).ready(function () {
 
             //reinicializar el slider
             SliderInit()
-
         }
 
-
+        /**
+         * Funcion que instancia el intervalo que tendra en slider
+         */
         const SliderInit = () => {
 
             //establecemos el valor de la variable
             SliderIntervar = setInterval(slider.startSlider, slider.settings.duration);
         }
 
-        slider.startSlider = function () {
-
-            /*VARIABLES NECESARIAS*/
-
-            //obtenemos los paneles del slider
-            var paneles = slider.items.panels
-
-            //boton de controles para el slider
-            let controles = $("#control-buttons li")
-
-            //si llega al final del slider(o se pasa)
-            if (nextSlider >= lengthSlider) {
-
-                //ponemos que el siguiente slider sera el del inicio y que estamos en el ultimo slider
-                nextSlider = 0
-                currentSlider = lengthSlider - 1
-            }
-
-            /*EFECTOS DE MOVIMIENTO*/
-
-            //PARA LOS CONTROLES
-
-            //quitamos la clase a todos los controles
-            controles.removeClass("active")
-            //al que veremos a continuacion, le ponemos la clase para que se coloree
-            controles.eq(nextSlider).addClass("active")
-
-            //PARA LOS PANELES
-
-            //quitamos la que tenemos visible
-            paneles.eq(currentSlider).fadeOut("slow")
-            //mostramos la que se supone que hemos pinchado
-            paneles.eq(nextSlider).fadeIn("slow")
-
-            /*ACTUALIZACION DE VARIABLES*/
-
-            //actualizamos el slider actual(movimiento hacia siguiente slider)
-            currentSlider = nextSlider
-
-            //actualizamos el valor del proximo slider
-            nextSlider++
-        }
-
-        let anteriorSlider = SliderIntervar
-
-        let contador = 0
-
-        slider.items.panels.hover(function () {
+        /* PARA PARAR EL INTERVALO Y QUE NO CAMBIE LA FOTO */
+        $("#slider>ul>li").hover(function () {
             // over
-
+            clearInterval(SliderIntervar)
         }, function () {
             // out
-
+            SliderInit()
         }
         );
+
+        /* FUNCIONALIDAD BOTONES */
+        $("#boton-izquierda").click(function (e) {
+
+            cambiarPanel(currentSlider - 1);
+        });
+
+        $("#boton-derecha").click(function (e) {
+
+            cambiarPanel(currentSlider + 1);
+        });
 
         return slider
 
